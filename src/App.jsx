54b0ae854096d3c09884ea975c6265df3bb442b6
Akshar-Saxena <aksharsaxena2003@gmail.com>
2023-10-22 10:56:15 +0530

    banner image done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import './App.css'
 import NavBar from './MyComponents/NavBar'
 import SearchBar from './MyComponents/SearchBar'
+import ViewButton from './MyComponents/ViewButton'
+import freeShipping from '../src/assets/icons/freeShipping.png'
+import refund from '../src/assets/icons/refund.png'
+import support from '../src/assets/icons/support.png'
+import mainImage from '../src/assets/chairs/mainImage.png'
 
 function App() {
   return (
     <>
       <NavBar/>
       <SearchBar/>
+      <div className="banner">
+        <div className="iconText">
+          <img src={freeShipping} alt="" />
+          <h3>Free Shipping</h3>
+        </div>
+        <div className="iconText">
+          <img src={refund} alt="" />
+          <h3>100% Refund</h3>
+        </div>
+        <div className="iconText">
+          <img src={support} alt="" />
+          <h3>Support 24/7</h3>
+        </div>
+      </div>
+      <div className="main">
+        <img src={mainImage} alt="" />
+        <div className="content">
+          <h3>HOT DEALS THIS WEEK</h3>
+          <h1>SALE 50% OFF MODERN FURNITURE</h1>
+          <ViewButton title="VIEW NOW"/>
+        </div>
+      </div>
+      <h2>...</h2>
     </>
   )
 }
--- src/MyComponents/SearchBar.jsx
+++ src/MyComponents/SearchBar.jsx
@@ -3,11 +3,14 @@ import magGlass from '../assets/icons/magnifyingGlass.png'
 
 export default function SearchBar() {
   return (
+    <div className="container">
     <div className='searchBar'>
       <div className="textField">
         <img src={magGlass} alt="" />
         <input type="text"/>
       </div>
+    </div>
+    <button id="search">Search</button>
     </div>
   )
 }
